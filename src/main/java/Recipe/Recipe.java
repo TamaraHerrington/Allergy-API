914f9cc60ee92b0ed69f873b284cf9f958a5f11d
TamaraHerrington <tamaraherrington24@gmail.com>
2021-11-03 15:28:48 +0000

    created Recipe class

---
+++ src/main/java/Recipe/Recipe.java
@@ -0,0 +1,11 @@
+package Recipe;
+
+public class Recipe {
+    private Integer id;
+    private String recipe;
+    private String dietryRequirement;
+    private Boolean shellfish;
+    private Boolean salicylates;
+    private Boolean vegan;
+  //  private enum diet;
+}
