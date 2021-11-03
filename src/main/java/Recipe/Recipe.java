d0b78f903d69b5811713f9b38ece6940a5bf4940
TamaraHerrington <tamaraherrington24@gmail.com>
2021-11-03 15:39:20 +0000

    finish creating Recipe class

--- src/main/java/Recipe/Recipe.java
+++ src/main/java/Recipe/Recipe.java
@@ -9,5 +9,63 @@ public class Recipe {
     private Boolean shellfish;
     private Boolean salicylates;
     private Boolean vegan;
-  //  private enum diet;
+    //  private enum diet;
+
+
+    public Recipe(Integer id, String recipe, List<String> dietaryRequirement, Boolean shellfish, Boolean salicylates, Boolean vegan) {
+        this.id = id;
+        this.recipe = recipe;
+        this.dietaryRequirement = dietaryRequirement;
+        this.shellfish = shellfish;
+        this.salicylates = salicylates;
+        this.vegan = vegan;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(String recipe) {
+        this.recipe = recipe;
+    }
+
+    public List<String> getDietaryRequirement() {
+        return dietaryRequirement;
+    }
+
+    public void setDietaryRequirement(List<String> dietaryRequirement) {
+        this.dietaryRequirement = dietaryRequirement;
+    }
+
+    public Boolean getShellfish() {
+        return shellfish;
+    }
+
+    public void setShellfish(Boolean shellfish) {
+        this.shellfish = shellfish;
+    }
+
+    public Boolean getSalicylates() {
+        return salicylates;
+    }
+
+    public void setSalicylates(Boolean salicylates) {
+        this.salicylates = salicylates;
+    }
+
+    public Boolean getVegan() {
+        return vegan;
+    }
+
+    public void setVegan(Boolean vegan) {
+        this.vegan = vegan;
+    }
 }
